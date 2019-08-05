2969c86b61848222cce59105bbb739469a3c79c5
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-08-05 20:52:15 +0700

    rabu:

---
+++ rabu/palindrome.js
@@ -0,0 +1,17 @@
+function palindrome(kata){
+    var kata1 = ''
+    for(var i=kata.length-1 ; i >=0 ; i-- ){
+        kata1+=kata[i]
+    }
+    if ( kata1 === kata ){
+        return hasil=true
+    }else {
+        return hasil=false
+    }
+}
+
+console.log(palindrome('katak'))
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
